81d52f4279d67817946e38660d483f759d8da91d
E. Belousov <visneviy.secret@yandex.ru>
2026-01-28 21:50:51 +0300

    Fix region

--- src/s3Client.ts/s3Client.ts
+++ src/s3Client.ts/s3Client.ts
@@ -1,7 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  region: process.env.S3_REGION || "ru-central1",
+  region: process.env.S3_REGION || "us-east-1",
   endpoint: process.env.S3_PUBLIC_ENDPOINT || "http://localhost:9000",
   credentials: {
     accessKeyId: process.env.S3_ACCESS_KEY || "minioadmin",
